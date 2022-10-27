826fcf08a121418b7d6d117584ab5ec206bc6257
aminespinoza10 <amin.espinoza@outlook.com>
2022-10-27 12:07:21 -0500

    web app project added

---
+++ webAppTemplate/main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "service_plan" {
+  name                = var.app_plan_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
+
+resource "azurerm_linux_web_app" "web_app" {
+  name                = var.app_service_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.service_plan.location
+  service_plan_id     = azurerm_service_plan.service_plan.id
+
+
+  site_config {
+      application_stack {
+        dotnet_version = "6.0"
+      } 
+  }
+}
